6c503f1669be565552bd862873ee289910089043
Falaventho <falaventho@gmail.com>
2024-10-04 18:23:25 -0400

    Changed u16 to i16, marie has signed ints.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{ self, Write };
+
 const AC: usize = 0; //Accumulator
 const PC: usize = 1; //Program Counter
 const IR: usize = 2; //Instruction Register
@@ -6,113 +8,116 @@ const MBR: usize = 4; //Memory Buffer Register
 const INPUT: usize = 5;
 const OUTPUT: usize = 6;
 
-
 struct CPU {
-    registers: [u16; 7],
+    registers: [i16; 7],
 }
 
 impl CPU {
+    fn new() -> CPU {
+        CPU {
+            registers: [0; 7],
+        }
+    }
 
     fn fetch(&mut self, memory: &Memory) {
         self.registers[MAR] = self.registers[PC];
-        self.registers[IR] = memory.read(self.registers[MAR] as usize);
+        self.registers[IR] = memory.read(self.registers[MAR]);
         self.registers[PC] += 1;
     }
 
-
     fn decode_execute(&mut self, memory: &mut Memory) {
-        match self.registers[IR] >> 12 { //Get opcode from 4 high bits
+        match self.registers[IR] >> 12 {
+            //Get opcode from 4 high bits
 
             //JnS
             0x0 => {
-                self.registers[MAR] = self.registers[IR] & 0x0FFF;
+                self.registers[MAR] = self.registers[IR] & 0x0fff;
                 self.registers[MBR] = self.registers[PC];
                 memory.write(self.registers[MAR], self.registers[MBR]);
                 self.registers[PC] = self.registers[IR] + 1;
-            },
+            }
 
             //LOAD
             0x1 => {
-                self.registers[MAR] = self.registers[IR] & 0x0FFF;
-                self.registers[AC] = memory.read(self.registers[MAR])
-            },
+                self.registers[MAR] = self.registers[IR] & 0x0fff;
+                self.registers[AC] = memory.read(self.registers[MAR]);
+            }
 
             //STORE
             0x2 => {
-                self.registers[MAR] = self.registers[IR] & 0x0FFF;
+                self.registers[MAR] = self.registers[IR] & 0x0fff;
                 self.registers[MBR] = self.registers[AC];
                 memory.write(self.registers[MAR], self.registers[MBR]);
-            },
+            }
 
             //ADD
             0x3 => {
-                self.registers[MAR] = self.registers[IR] & 0x0FFF;
+                self.registers[MAR] = self.registers[IR] & 0x0fff;
                 self.registers[MBR] = memory.read(self.registers[MAR]);
                 self.registers[AC] += self.registers[MBR];
-            },
+            }
 
             //SUBT
             0x4 => {
-                self.registers[MAR] = self.registers[IR] & 0x0FFF;
+                self.registers[MAR] = self.registers[IR] & 0x0fff;
                 self.registers[MBR] = memory.read(self.registers[MAR]);
                 self.registers[AC] -= self.registers[MBR];
-            },
+            }
 
             //INPUT
             0x5 => {
                 self.input();
-            },
+            }
 
             //OUTPUT
             0x6 => {
                 self.output();
-            },
+            }
 
             //HALT
             0x7 => {
                 std::process::exit(0);
-            },
+            }
 
             //SKIPCOND
             0x8 => {
-                let cond = self.registers[IR] & 0x0C00 >> 10; //Get condition from bits 11-10 and shift to LSB for easy comparison
+                let cond = self.registers[IR] & (0x0c00 >> 10); //Get condition from bits 11-10 and shift to LSB for easy comparison
                 match cond {
-
                     0b00 => {
                         if self.registers[AC] < 0 {
                             self.registers[PC] += 1;
                         }
-                    },
+                    }
 
                     0x01 => {
                         if self.registers[AC] == 0 {
                             self.registers[PC] += 1;
                         }
-                    },
+                    }
 
                     0b10 => {
                         if self.registers[AC] > 0 {
                             self.registers[PC] += 1;
                         }
-                    },
+                    }
                     _ => {}
                 }
-            },
+            }
 
             //JUMP
             0x9 => {
-                self.registers[PC] = self.registers[IR] & 0x0FFF;
-            },
+                self.registers[PC] = self.registers[IR] & 0x0fff;
+            }
 
             //CLEAR
-            0xA => {
+            0xa => {
                 self.registers[AC] = 0x0000;
-            },
+            }
 
             //ADDI
-            0xB => {
+            0xb => {
                 // read indirect
-                self.registers[MAR] = self.registers[IR] & 0x0FFF;
+                self.registers[MAR] = self.registers[IR] & 0x0fff;
                 self.registers[MBR] = memory.read(self.registers[MAR]);
                 self.registers[MAR] = self.registers[MBR];
                 self.registers[MBR] = memory.read(self.registers[MAR]);
@@ -120,16 +125,16 @@ impl CPU {
             }
 
             //JUMPI
-            0xC => {
-                self.registers[MAR] = self.registers[IR] & 0x0FFF;
+            0xc => {
+                self.registers[MAR] = self.registers[IR] & 0x0fff;
                 self.registers[MBR] = self.registers[MAR];
-                self.registers[PC] =self.registers[MBR];
+                self.registers[PC] = self.registers[MBR];
             }
 
             //LOADI
-            0xD => {
+            0xd => {
                 // read indirect
-                self.registers[MAR] = self.registers[IR] & 0x0FFF;
+                self.registers[MAR] = self.registers[IR] & 0x0fff;
                 self.registers[MBR] = memory.read(self.registers[MAR]);
                 self.registers[MAR] = self.registers[MBR];
                 self.registers[MBR] = memory.read(self.registers[MAR]);
@@ -137,20 +142,18 @@ impl CPU {
             }
 
             //STOREI
-            0xE => {
+            0xe => {
                 // write indirect
-                self.registers[MAR] = self.registers[IR] & 0x0FFF;
+                self.registers[MAR] = self.registers[IR] & 0x0fff;
                 self.registers[MBR] = memory.read(self.registers[MAR]);
                 self.registers[MAR] = self.registers[MBR];
                 self.registers[MBR] = self.registers[AC];
                 memory.write(self.registers[MAR], self.registers[MBR]);
-            },
+            }
 
             _ => {}
-
         }
     }
-
 
     fn input(&mut self) {
         let mut input = String::new();
@@ -165,19 +168,15 @@ impl CPU {
     }
 
     fn run(&mut self, memory: &mut Memory) {
-            loop {
-                self.fetch(memory);
-                self.decode_execute(memory);
-            }
+        loop {
+            self.fetch(memory);
+            self.decode_execute(memory);
+        }
     }
-    
-
-
 }
 
-
 struct Memory {
-    mem: Vec<u16>
+    mem: Vec<i16>,
 }
 
 impl Memory {
@@ -187,41 +186,38 @@ impl Memory {
         }
     }
 
-    fn read(&self, address:u16) -> u16 {
-        self.mem[address as usize]   
+    fn read(&self, address: i16) -> i16 {
+        self.mem[address as usize]
     }
 
-    fn write(&mut self, address: u16, value: u16) {
+    fn write(&mut self, address: i16, value: i16) {
         self.mem[address as usize] = value;
     }
 
-    fn load_program(&mut self, program: Vec<u16>) {
+    fn load_program(&mut self, program: Vec<i16>) {
         for (i, &instruction) in program.iter().enumerate() {
-            self.write(i as u16, instruction);
+            self.write(i as i16, instruction);
         }
     }
-
-
 }
 
 enum Instruction {
-    JnS(u16),
-    Load(u16),
-    Store(u16),
-    Add(u16),
-    Subt(u16),
+    JnS(i16),
+    Load(i16),
+    Store(i16),
+    Add(i16),
+    Subt(i16),
     Input,
     Output,
-    Halt
-    Skipcond(u16),
-    Jump(u16),
+    Halt,
+    Skipcond(i16),
+    Jump(i16),
     Clear,
     AddI,
     JumpI,
     LoadI,
-    StoreI
+    StoreI,
 }
-
 
 fn main() {
     let mut memory = Memory::new(4096);
@@ -250,20 +246,20 @@ fn main() {
         0x6100, // Output AC
         0x1109, // Load d (ASCII 100) into AC
         0x6100, // Output AC
-        0x110A, // Load ! (ASCII 33) into AC
+        0x110a, // Load ! (ASCII 33) into AC
         0x6100, // Output AC
         0x7000, // Halt
         // Data section
         0x0048, // H (ASCII 72)
         0x0065, // e (ASCII 101)
-        0x006C, // l (ASCII 108)
-        0x006F, // o (ASCII 111)
-        0x002C, // , (ASCII 44)
+        0x006c, // l (ASCII 108)
+        0x006f, // o (ASCII 111)
+        0x002c, // , (ASCII 44)
         0x0020, // space (ASCII 32)
         0x0057, // W (ASCII 87)
         0x0072, // r (ASCII 114)
         0x0064, // d (ASCII 100)
-        0x0021, // ! (ASCII 33)
+        0x0021 // ! (ASCII 33)
     ];
     memory.load_program(program);
 
